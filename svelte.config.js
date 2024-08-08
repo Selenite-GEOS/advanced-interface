100f00349210b0f002ae2ba1cfeb1a45098b9e6e
MoonLyss <shaitan.lyssm@gmail.com>
2024-08-08 11:37:40 +0200

    Fx typo

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const base = process.env.BASE_PATH || ''
-console.log("BASE_PAHT", base)
+console.log("BASE_PATH", base)
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
