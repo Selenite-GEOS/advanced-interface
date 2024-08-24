a9afa945ea8c73449bef9c9ddbd3458008f99321
MoonLyss <shaitan.lyssm@gmail.com>
2024-08-24 14:14:39 +0200

    Improve fullscreen

--- src/lib/graph-editor/buttons/FullscreenButton.svelte
+++ src/lib/graph-editor/buttons/FullscreenButton.svelte
@@ -1,33 +1,43 @@
-<script>
+<script lang="ts">
 	import { faMaximize, faCompress } from '@fortawesome/free-solid-svg-icons';
 	import EditorButton from '../EditorButton.svelte';
 	import { getCurrentWindow } from '@tauri-apps/api/window';
 	import { isTauri } from '@tauri-apps/api/core';
-	import { sleep } from '@selenite/commons';
+	import { notifications } from '@selenite/graph-editor';
 	let fullscreen = $state(false);
 
 	function onFullscreenChange() {
-		console.debug("Fullscreen change", document.fullscreenElement);
+		console.debug('Fullscreen change', document.fullscreenElement);
 		fullscreen = !!document.fullscreenElement;
 	}
 
+	function preventF11(e: KeyboardEvent) {
+		if (e.key === 'F11') {
+			console.log('Preventing F11');
+			notifications.warn({message: 'F11 has been disabled for fullscreen, use F10 or Alt+Enter instead.', title: 'Fullscreen'});
+			e.preventDefault();
+			e.stopPropagation();
+			e.stopImmediatePropagation();
+		}
+	}
 	$effect(() => {
 		if (!isTauri()) {
-		document.addEventListener('fullscreenchange', onFullscreenChange);
-		return () => document.removeEventListener('fullscreenchange', onFullscreenChange);
+			document.addEventListener('keydown', preventF11);
+			window.addEventListener('keydown', preventF11);
+			document.addEventListener('fullscreenchange', onFullscreenChange);
+			return () => {
+				document.removeEventListener('fullscreenchange', onFullscreenChange);
+				document.removeEventListener('keydown', preventF11);
+				window.removeEventListener('keydown', preventF11);
+			};
 		}
-	})
-
-	// $effect(() => {
-	// 	document.addEventListener('fullscreenchange', onFullscreenChange);
-	// 	return () => document.removeEventListener('fullscreenchange', onFullscreenChange);
-	// });
+	});
 </script>
 
 <EditorButton
 	label="Fullscreen"
 	description={`${fullscreen ? 'Leave' : 'Enter'} full screen mode.`}
-	shortcut="f11"
+	shortcut={['f10', 'alt+enter']}
 	icon={fullscreen ? faCompress : faMaximize}
 	onclick={async (e) => {
 		e.preventDefault();
@@ -37,11 +47,14 @@
 		} else {
 			window.focus();
 			if (document.fullscreenElement === null) {
+				try {
 				await document.documentElement.requestFullscreen();
+				} catch (e) {
+					notifications.warn({message: "Can't enter fullscreen right now, try interacting with the page first.", title: 'Fullscreen'})
+				}
 			} else {
 				await document.exitFullscreen();
 			}
-			fullscreen = document.fullscreenElement !== null;
 		}
 	}}
 />
